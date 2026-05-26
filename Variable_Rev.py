1d939868b35cc54b0bb6a3e1d208912ac940a082
riyayadav82913-wq <riyayadav82913@gmail.com>
2026-05-26 18:19:31 +0530

    Add files via upload
    Python learning and practice repository.

---
+++ Variable_Rev.py
@@ -0,0 +1,12 @@
+# Variable Revision
+
+subject1="Maths"
+subject2="Biology"
+
+bio_marks=40
+maths_marks=88
+
+if bio_marks> maths_marks:
+    print(subject1 , "has higher marks:",maths_marks, "out of 100")
+else:
+    print(subject2, "has higher marks:", bio_marks, "out of 100")
