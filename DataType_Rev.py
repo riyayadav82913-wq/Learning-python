f1e706323867f1d1070e0b38f0b5df9416048180
riyayadav82913-wq <riyayadav82913@gmail.com>
2026-05-27 17:26:26 +0530

    Add files via upload
    Python Data Tyes practice

---
+++ DataType_Rev.py
@@ -0,0 +1,47 @@
+# Data Types
+
+# Q.1 Print all data Types
+
+name = "Sonali"
+age = 19
+height = 5.7
+is_student= True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+# Q.2 Take user input( name , age ) and print it
+
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+
+print("Name ->", name)
+print("Age ->", age)
+
+# Q.3 Create a variable for price using float data type and print it.
+
+Fan_price = 1500.45
+print("The price of the Fan is :", Fan_price)
+
+# Q.4 Create a boolean variable to show login status and print it.
+
+is_logged_in = True
+print(is_logged_in)
+
+# Q.5 Create a city name (string) and pincode (integer) and print both.
+
+City = "Mumbai"
+Pincode = "400067"
+
+print(City)
+print(Pincode)
+
+#Q.6 Check and print the data types of x = 10 and y = "10"
+
+x = 10
+y ="10"
+print(type(x))
+print(type(y))
+
